5731de76a550e741620b06017e3d93203460743e
Pablo Vila Mauricio <pvilamauricio@danielcastelao.org>
2021-10-15 09:43:23 +0200

    Cambiado el mensaje

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -2,6 +2,6 @@ package com.dam1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo ad");
+        System.out.println("Hola mundo adc");
     }
 }
